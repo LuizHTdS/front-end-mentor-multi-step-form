561a0f4dbdd249e416d1b867d908f3069b01753a
Luiz T <90431370+LuizHTdS@users.noreply.github.com>
2023-01-28 11:08:44 -0300

    formatting

--- src/components/CurrentStepCard.tsx
+++ src/components/CurrentStepCard.tsx
@@ -1,11 +1,18 @@
+import { Step } from "./Step";
+
 type Props = {
     curStep: number;
 }
 
-export const CurrentStepCard : React.FC<Props> = (props) => {
+
+export const CurrentStepCard: React.FC<Props> = (props) => {
+    const stepElemets = ['YOUR INFO', 'SELECT PLAN', 'ADD-ONS', 'SUMMARY'].map((action, i) => {
+        return <Step step={i + 1} action={action} curStep={props.curStep} />
+    })
+
     return (
         <aside className="curStepCard">
-            
+            {stepElemets}
         </aside>
     )
 }
