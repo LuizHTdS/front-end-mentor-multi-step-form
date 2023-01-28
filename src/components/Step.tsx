125fe71b8e7fa0c76b8abc6901835a32ec768175
Luiz T <90431370+LuizHTdS@users.noreply.github.com>
2023-01-28 10:31:30 -0300

    current step card markdown finished

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,12 +1,14 @@
 type StepProps = {
     step: number;
     action: string;
+    curStep: number;
 }
 
 export const Step = (props: StepProps) => {
     return (
+
         <div className="curStepCard__step">
-            <div className="curStepCard__step--number">
+            <div className={`curStepCard__step--number ${props.curStep && 'active'}`}>
                 <h2>{props.step}</h2>
             </div>
             <h3 className="curStepCard__step--action">
