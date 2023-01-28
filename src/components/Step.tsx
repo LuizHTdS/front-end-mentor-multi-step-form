1ca90bbac2d2ab15b73306811672686659b10da4
Luiz T <90431370+LuizHTdS@users.noreply.github.com>
2023-01-28 07:59:11 -0300

    finished card markdown

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,4 +1,4 @@
-interface StepProps {
+type StepProps = {
     step: number;
     action: string;
 }
@@ -6,7 +6,15 @@ interface StepProps {
 export const Step = (props: StepProps) => {
     return (
         <div className="curStepCard__step">
-            {props.step}
+            <div className="curStepCard__step--number">
+                <h2>{props.step}</h2>
+            </div>
+            <h3 className="curStepCard__step--action">
+                <span className="curStepCard__step--action-step">
+                    STEP {props.step}
+                </span>
+                {props.action}
+            </h3>
         </div>
     )
 }
