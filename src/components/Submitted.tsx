20a41f2d48efa0253abf3aa7be152115eac73492
Luiz T <90431370+LuizHTdS@users.noreply.github.com>
2023-03-14 21:40:43 -0300

    finished submit styling

--- src/components/Submitted.tsx
+++ src/components/Submitted.tsx
@@ -1,13 +1,15 @@
 const Submitted = () => {
   return (
-    <div className='submited'>
-      <img src='images/icon-thank-you.svg' alt='a red checkmark icon' />
-      <h1>Thank you!</h1>
-      <p>
-        Thanks for confirming your subscription! We hope you have fun using our
-        platform. If you ever need suport, please feel free to email us at
-        support@loremgaming.com
-      </p>
+    <div className='submitted'>
+      <div className='submitted__container'>
+        <img src='images/icon-thank-you.svg' alt='a red checkmark icon' />
+        <h1>Thank you!</h1>
+        <p>
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need suport, please feel free to email us at
+          support@loremgaming.com
+        </p>
+      </div>
     </div>
   );
 };
